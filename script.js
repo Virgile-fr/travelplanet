fe1759ea2ede220dfc62d8a4282ffcd973186671
Virgile Arlaud <virgile.arlaud@gmail.com>
2022-04-06 16:07:50 +0200

    fix JSON fetch / add download json fuction / review code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// SELECTORS ///
+// 📌 SELECTORS
 let id = (id) => document.getElementById(id);
 const erreur = id("error");
 const goodC = id("good");
@@ -8,7 +8,7 @@ const site = id("site");
 const pwd = id("pwd");
 const submit = id("submit");
 
-//SUBMIT ACTION -> GET TOKEN //
+// 👁 SUBMIT ➡️ 💾 CREDENTIALS ➡️ 🚀 getToken W credentials
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let password = pwd.value;
@@ -17,66 +17,10 @@ form.addEventListener("submit", (e) => {
   getToken(password, siteID, userID);
 });
 
-// ASK FOR JSON BUTTON -> FETCH JSON //
-function askForJson() {
-  const travelButton = id("travelbutton");
-  travelButton.addEventListener("click", () => {
-    // console test // test the token format for traveler info fetching
-    console.log({ access_token: JSON.parse(localStorage.getItem("data")) });
-    // set request options
-    const optionsGetJson = {
-      method: "GET",
-      Params: { access_token: JSON.parse(localStorage.getItem("data")) },
-      headers: { "Content-Type": "application/json" }, };
-    // set request url
-    const urlGetJson = "https://api.qal.travelplanet.click/profils_v2/v1/profil/booking-rules/travellers-list/";
-    // request the JSON
-    fetch(urlGetJson, optionsGetJson)
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  });
-}
-
-// SHOW HTML ERROR - ERASE GOOD CREDENTIALS //
-function error() {
-  goodC.classList.remove("show");
-  goodC.classList.add("hide");
-  erreur.classList.remove("hide");
-  erreur.classList.add("show");
-}
-
-// HIDE LOGIN / SHOW GOOD CREDENTIALS MESSAGE / SHOW ASK FOR JSON BUTTON / CALL LISTENER FUNCTION ON BUTTON
-function good(name) {
-  erreur.classList.remove("show");
-  erreur.classList.add("hide");
-  form.classList.add("hide");
-  goodC.classList.remove("hide");
-  goodC.classList.add("show");
-  goodC.insertAdjacentHTML(
-  "beforeend",
-  `<h1><b>👋 Hello.</b> ${user.value} </h1>
-  <p>connection réussie</p>
-  <button id='travelbutton'>Ask for Json</button>`
-  );
-  askForJson();
-}
-
-// FUNCTION IF FETCHING ERROR --> EXECUTE SHOW HTML ERROR
-function handleErrors(response) {
-  if (!response.ok) {
-    error();
-    throw Error(response.statusText);
-  } else { good();
-    return response;
-  }
-}
-
-// GET TOKEN FUNCTION
+// OBTENTION TOKEN ➡️ 💾 TOKEN in LOCAL STORAGE ➡️ 🚀 ifNoError
 function getToken(password, siteID, userID) {
-  //credentials test
-  console.log(siteID);
-  console.log(userID);
-  console.log(password);
+  //show credentials on console
+  console.log(siteID); console.log(userID); console.log(password);
   //set token fetch options
   const optionsGetToken = {
     method: "POST",
@@ -85,7 +29,7 @@ function getToken(password, siteID, userID) {
       user_id: userID,
       password: password,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json" }
   };
   //set token fetch url
   const urlGetToken = "https://api.qal.travelplanet.click/auth/v1/click";
@@ -96,10 +40,81 @@ function getToken(password, siteID, userID) {
     .then((json) => localStorage.setItem("data", JSON.stringify(json.token)));
     // store token in variable
     const data = JSON.parse(localStorage.getItem("data"));
-  // FETCH TOKEN ERROR
+  // fetch for errors
   fetch(urlGetToken, optionsGetToken)
-    // call function handleErrors
-    .then(handleErrors)
+    // call function ifNoError
+    .then(ifNoError)
     .then((response) => console.log("ok"))
     .catch((error) => console.log(error));
-} //end function getToken
+}
+
+// if error ➡️ 🚀 error // if no error ➡️ 🚀 showAskJson
+function ifNoError(response) {
+  if (!response.ok) {
+    error();
+    throw Error(response.statusText);
+  } else { showAskJson();
+    return response;
+  }
+}
+
+// 💥 Show error message
+function error() {
+  goodC.classList.remove("show");
+  goodC.classList.add("hide");
+  erreur.classList.remove("hide");
+  erreur.classList.add("show");
+}
+
+// 💥 Show Ask Json page ➡️ 🚀 AskForJson
+function showAskJson(name) {
+  erreur.classList.remove("show");
+  erreur.classList.add("hide");
+  form.classList.add("hide");
+  goodC.classList.remove("hide");
+  goodC.classList.add("show");
+  goodC.insertAdjacentHTML(
+    "beforeend",
+    `<h1><b>👋 Hello.</b> ${user.value} </h1>
+  <p>connection réussie</p>
+  <button id='travelbutton'>Ask for Json</button>`
+  );
+  askForJson();
+}
+
+// 👁 ask json button ➡️ 🚀 AskForJsonUrl
+function askForJson() {
+  const travelButton = id("travelbutton");
+  travelButton.addEventListener("click", () => {
+    askForJsonUrl()
+  });
+}
+
+// 💾 Json url ➡️ 🚀 download W url
+function askForJsonUrl() {
+    let tokenString = JSON.parse(localStorage.getItem("data"));
+    const params = `access_token=${tokenString}`;
+    const urlGetJson = `https://api.qal.travelplanet.click/profils_v2/v1/profil/booking-rules/travellers-list?${params}`;
+    download(urlGetJson);
+};
+
+// 💥💾 JSON
+function download(url) {
+  fetch(url)
+    .then(async (response) => {
+      let clone = response.clone();
+      let res = await clone.json();
+      console.log(res);
+      return response.blob();
+    })
+    .then((blob) => {
+      var url = window.URL.createObjectURL(blob);
+      var a = document.createElement("a");
+      a.href = url;
+      a.download = `${user.value}'s travellers.json`;
+      a.click();
+    })
+    .catch(function (err) {
+      console.error(err);
+    });
+};
